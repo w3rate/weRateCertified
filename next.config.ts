c05a0a443c009069ec2d77675c3582c28c980c9a
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2025-04-29 15:10:51 +0500

    feat: set up next app

---
+++ next.config.ts
@@ -0,0 +1,56 @@
+import type { NextConfig } from "next";
+
+async function getHeaders() {
+  const headers = [];
+
+  headers.push({
+    source: '/:path*',
+    headers: [
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN',
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'strict-origin-when-cross-origin',
+      },
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+      },
+      {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()',
+      },
+      {
+        key: 'Content-Security-Policy', 
+        value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';"
+      }
+    ],
+  });
+
+  return headers;
+}
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+    reactRemoveProperties: process.env.NODE_ENV === 'production' ? { properties: ['^data-testid$'] } : false,
+  },
+
+  experimental: {
+    optimizePackageImports: [],
+  },
+
+  headers: getHeaders,
+};
+
+export default nextConfig;
